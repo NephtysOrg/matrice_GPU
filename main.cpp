65082705b59f1d93430048da2a3d8e747914cc65
Follet Charles <cfollet@scinfe052.univ-pau.fr>
2015-03-26 17:43:18 +0100

    last commit

--- main.cpp
+++ main.cpp
@@ -13,9 +13,11 @@ const int leafsize = 1024;
 QCLVector<int> inbuffer_A;
 QCLVector<int> inbuffer_B;
 QCLVector<int> outbuffer;
+
 QCLKernel kernel;
 QCLContext context;
 QCLProgram program;
+
 int mode = CPU;
 
 void multiply(vector< vector<int> > A, vector< vector<int> > B, vector< vector<int> > &C, int n);
@@ -24,7 +26,8 @@ void CPUMult(vector< vector<int> > A, vector< vector<int> > B, vector< vector<in
 void GPUMult(vector< vector<int> > A, vector< vector<int> > B, vector< vector<int> > &C, int n);
 void strassen(vector< vector<int> > &A, vector< vector<int> > &B, vector< vector<int> > &C, unsigned int tam);
 unsigned int nextPowerOfTwo(int n);
-void strassenR(vector< vector<int> > &A, vector< vector<int> > &B, vector< vector<int> > &C, int tam);
+void strassenCPU(vector< vector<int> > &A, vector< vector<int> > &B, vector< vector<int> > &C, int tam);
+void strassenGPU(vector< vector<int> > &A, vector< vector<int> > &B, vector< vector<int> > &C, int tam);
 void sum(vector< vector<int> > &A, vector< vector<int> > &B, vector< vector<int> > &C, int tam);
 void subtract(vector< vector<int> > &A, vector< vector<int> > &B, vector< vector<int> > &C, int tam);
 void printMatrix(vector< vector<int> > matrix);
@@ -49,15 +52,6 @@ vector<int> matrixPlane(vector< vector<int> > A,bool col){
 }
 
 
-void multiply(vector< vector<int> > A,
-              vector< vector<int> > B,
-              vector< vector<int> > &C, int n){
-    if(mode == CPU){
-        CPUMult(A,B,C,n);
-    }else{
-        GPUMult(A,B,C,n);
-    }
-}
 
 
 void CPUMult(vector< vector<int> > A,
@@ -75,7 +69,6 @@ void CPUMult(vector< vector<int> > A,
 void GPUMult(vector< vector<int> > A,
              vector< vector<int> > B,
              vector< vector<int> > &C, int n) {
-
     vector<int> A_plane = matrixPlane(A,false);
     vector<int> B_plane = matrixPlane(B,true);
     vector<int> outdata(A_plane.size());
@@ -97,7 +90,6 @@ void GPUMult(vector< vector<int> > A,
     inbuffer_B.write(&B_plane[0],B_plane.size());
 
     kernel.run();
-    context.sync();
 
     outbuffer.read(&outdata[0],A_plane.size());
 
@@ -114,12 +106,11 @@ void GPUMult(vector< vector<int> > A,
 }
 
 
-
-void strassenR(vector< vector<int> > &A,
-               vector< vector<int> > &B,
-               vector< vector<int> > &C, int tam) {
+void strassenGPU(vector< vector<int> > &A,
+                 vector< vector<int> > &B,
+                 vector< vector<int> > &C, int tam) {
     if (tam <= leafsize) {
-        multiply(A, B, C, tam);
+        GPUMult(A, B, C, tam);
         return;
     }
 
@@ -157,27 +148,117 @@ void strassenR(vector< vector<int> > &A,
 
         sum(a11, a22, aResult, newTam); // a11 + a22
         sum(b11, b22, bResult, newTam); // b11 + b22
-        strassenR(aResult, bResult, p1, newTam); // p1 = (a11+a22) * (b11+b22)
+        strassenGPU(aResult, bResult, p1, newTam); // p1 = (a11+a22) * (b11+b22)
 
         sum(a21, a22, aResult, newTam); // a21 + a22
-        strassenR(aResult, b11, p2, newTam); // p2 = (a21+a22) * (b11)
+        strassenGPU(aResult, b11, p2, newTam); // p2 = (a21+a22) * (b11)
 
         subtract(b12, b22, bResult, newTam); // b12 - b22
-        strassenR(a11, bResult, p3, newTam); // p3 = (a11) * (b12 - b22)
+        strassenGPU(a11, bResult, p3, newTam); // p3 = (a11) * (b12 - b22)
 
         subtract(b21, b11, bResult, newTam); // b21 - b11
-        strassenR(a22, bResult, p4, newTam); // p4 = (a22) * (b21 - b11)
+        strassenGPU(a22, bResult, p4, newTam); // p4 = (a22) * (b21 - b11)
 
         sum(a11, a12, aResult, newTam); // a11 + a12
-        strassenR(aResult, b22, p5, newTam); // p5 = (a11+a12) * (b22)
+        strassenGPU(aResult, b22, p5, newTam); // p5 = (a11+a12) * (b22)
 
         subtract(a21, a11, aResult, newTam); // a21 - a11
         sum(b11, b12, bResult, newTam); // b11 + b12
-        strassenR(aResult, bResult, p6, newTam); // p6 = (a21-a11) * (b11+b12)
+        strassenGPU(aResult, bResult, p6, newTam); // p6 = (a21-a11) * (b11+b12)
 
         subtract(a12, a22, aResult, newTam); // a12 - a22
         sum(b21, b22, bResult, newTam); // b21 + b22
-        strassenR(aResult, bResult, p7, newTam); // p7 = (a12-a22) * (b21+b22)
+        strassenGPU(aResult, bResult, p7, newTam); // p7 = (a12-a22) * (b21+b22)
+
+        // calculating c21, c21, c11 e c22:
+
+        sum(p3, p5, c12, newTam); // c12 = p3 + p5
+        sum(p2, p4, c21, newTam); // c21 = p2 + p4
+
+        sum(p1, p4, aResult, newTam); // p1 + p4
+        sum(aResult, p7, bResult, newTam); // p1 + p4 + p7
+        subtract(bResult, p5, c11, newTam); // c11 = p1 + p4 - p5 + p7
+
+        sum(p1, p3, aResult, newTam); // p1 + p3
+        sum(aResult, p6, bResult, newTam); // p1 + p3 + p6
+        subtract(bResult, p2, c22, newTam); // c22 = p1 + p3 - p2 + p6
+
+        // Grouping the results obtained in a single matrix:
+        for (i = 0; i < newTam ; i++) {
+            for (j = 0 ; j < newTam ; j++) {
+                C[i][j] = c11[i][j];
+                C[i][j + newTam] = c12[i][j];
+                C[i + newTam][j] = c21[i][j];
+                C[i + newTam][j + newTam] = c22[i][j];
+            }
+        }
+    }
+}
+
+
+void strassenCPU(vector< vector<int> > &A,
+                 vector< vector<int> > &B,
+                 vector< vector<int> > &C, int tam) {
+    if (tam <= leafsize) {
+        CPUMult(A, B, C, tam);
+        return;
+    }
+
+    // other cases are treated here:
+    else {
+        int newTam = tam/2;
+        vector<int> inner (newTam,0);
+        vector< vector<int> >
+                a11(newTam,inner), a12(newTam,inner), a21(newTam,inner), a22(newTam,inner),
+                b11(newTam,inner), b12(newTam,inner), b21(newTam,inner), b22(newTam,inner),
+                c11(newTam,inner), c12(newTam,inner), c21(newTam,inner), c22(newTam,inner),
+                p1(newTam,inner), p2(newTam,inner), p3(newTam,inner), p4(newTam,inner),
+                p5(newTam,inner), p6(newTam,inner), p7(newTam,inner),
+                aResult(newTam,inner), bResult(newTam,inner);
+
+        int i, j;
+
+        //dividing the matrices in 4 sub-matrices:
+        for (i = 0; i < newTam; i++) {
+            for (j = 0; j < newTam; j++) {
+                a11[i][j] = A[i][j];
+                a12[i][j] = A[i][j + newTam];
+                a21[i][j] = A[i + newTam][j];
+                a22[i][j] = A[i + newTam][j + newTam];
+
+                b11[i][j] = B[i][j];
+                b12[i][j] = B[i][j + newTam];
+                b21[i][j] = B[i + newTam][j];
+                b22[i][j] = B[i + newTam][j + newTam];
+            }
+        }
+
+
+        // Calculating p1 to p7:
+
+        sum(a11, a22, aResult, newTam); // a11 + a22
+        sum(b11, b22, bResult, newTam); // b11 + b22
+        strassenCPU(aResult, bResult, p1, newTam); // p1 = (a11+a22) * (b11+b22)
+
+        sum(a21, a22, aResult, newTam); // a21 + a22
+        strassenCPU(aResult, b11, p2, newTam); // p2 = (a21+a22) * (b11)
+
+        subtract(b12, b22, bResult, newTam); // b12 - b22
+        strassenCPU(a11, bResult, p3, newTam); // p3 = (a11) * (b12 - b22)
+
+        subtract(b21, b11, bResult, newTam); // b21 - b11
+        strassenCPU(a22, bResult, p4, newTam); // p4 = (a22) * (b21 - b11)
+
+        sum(a11, a12, aResult, newTam); // a11 + a12
+        strassenCPU(aResult, b22, p5, newTam); // p5 = (a11+a12) * (b22)
+
+        subtract(a21, a11, aResult, newTam); // a21 - a11
+        sum(b11, b12, bResult, newTam); // b11 + b12
+        strassenCPU(aResult, bResult, p6, newTam); // p6 = (a21-a11) * (b11+b12)
+
+        subtract(a12, a22, aResult, newTam); // a12 - a22
+        sum(b21, b22, bResult, newTam); // b21 + b22
+        strassenCPU(aResult, bResult, p7, newTam); // p7 = (a12-a22) * (b21+b22)
 
         // calculating c21, c21, c11 e c22:
 
@@ -211,7 +292,6 @@ unsigned int nextPowerOfTwo(int n) {
 void strassen(vector< vector<int> > &A,
               vector< vector<int> > &B,
               vector< vector<int> > &C, unsigned int n) {
-    //unsigned int n = tam;
     unsigned int m = nextPowerOfTwo(n);
     vector<int> inner(m,0);
     vector< vector<int> > APrep(m, inner), BPrep(m, inner), CPrep(m, inner);
@@ -222,8 +302,12 @@ void strassen(vector< vector<int> > &A,
             BPrep[i][j] = B[i][j];
         }
     }
+    if(mode == GPU){
+        strassenGPU(APrep, BPrep, CPrep, m);
+    }else{
+        strassenCPU(APrep, BPrep, CPrep, m);
+    }
 
-    strassenR(APrep, BPrep, CPrep, m);
     for(unsigned int i=0; i<n; i++) {
         for (unsigned int j=0; j<n; j++) {
             C[i][j] = CPrep[i][j];
@@ -274,7 +358,6 @@ using namespace std;
 int main(int argc, char *argv[])
 {
     int TAILLE=10;
-    int mode = CPU;
     if(argv[2] != NULL)
         TAILLE=(atoi(argv[2])>0)?atoi(argv[2]):10;
     if(argv[1] != NULL){
@@ -289,10 +372,15 @@ int main(int argc, char *argv[])
             A[i][j] = B[i][j] = 1;
         }
     }
+    if(!context.create()){
+        qFatal("Could not create OpenCL context for the GPU\n");
+        exit(0);
+    }
 
     clock_t tStart = clock();
     strassen(A, B, C, TAILLE);
-    cout<<"time :"<<(clock() - tStart)/(double)(CLOCKS_PER_SEC/1000)<<" ms"<<endl;
+    cout<<"time :"<<(clock() - tStart)/(double)(CLOCKS_PER_SEC)<<" ms"<<endl;
+    cout<<"C"<<C[0][0]<<endl;
     if(TAILLE <= 5){
         printMatrix(C);
     }
